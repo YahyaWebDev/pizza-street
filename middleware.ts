889e25861c15b8182bb93a9a79a491ffaad66e81
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-12 02:59:38 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-// middleware.ts
-import { authMiddleware } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  publicRoutes: ['/', '/sign-in(.*)', '/api/webhook'],
-  debug: process.env.NODE_ENV === 'development'
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
