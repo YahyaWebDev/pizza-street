b0a77851cca6afc259dbd8fda368d1c1972836bd
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-12 02:57:11 +0200

    Update clerk.config.ts

--- clerk.config.ts
+++ clerk.config.ts
@@ -1,10 +1,9 @@
 // clerk.config.ts
 export default {
-  domain: 'pizza-street.vercel.app',
+  domain: process.env.NODE_ENV === 'production' 
+    ? 'pizza-street.vercel.app' 
+    : 'localhost',
   signInUrl: '/sign-in',
   isSatellite: true,
-  proxyUrl: '/api/clerk',
-  // DEVELOPMENT ONLY:
-  dangerouslyDisableSecureWhileTesting: true,
-  skipDomainCheck: true
+  proxyUrl: '/api/clerk'
 };
